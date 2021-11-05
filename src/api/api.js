b3407e10d935cd3cf237383c4c8b898875dba371
VictorOliveira02 <victoralvees17@gmail.com>
2021-11-05 10:15:05 -0300

    ajuste rotas

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
+//const baseURL = "http://localhost:5000/api";
+const baseURL = "https://mural-de-avisos-back-end.vercel.app/";
 
 const api = Axios.create({
     baseURL,
