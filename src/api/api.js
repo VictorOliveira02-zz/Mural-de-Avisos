69d4c4ea63ba6f337e659f9d649fe75cbf6048ee
VictorOliveira02 <victoralvees17@gmail.com>
2021-11-05 20:14:41 -0300

    ajustes

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
-
+//const baseURL = "http://localhost:5000/api";
+const baseURL = "https://mural-de-avisos-backend.herokuapp.com/api";
 const api = Axios.create({
     baseURL,
 });
